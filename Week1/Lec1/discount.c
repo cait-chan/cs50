0821a1470cde925a111aac8ac831d4a3bd513c79
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-08-12 06:03:31 +0000

    ddd, Aug 8/12/2022, YYYY, 6:03 A +0

---
+++ Week1/Lec1/discount.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    float regular = get_float("Regular Price: ");
+    float sale = regular * .85;
+    printf("Sale Price: %.2f\n", sale);
+}
