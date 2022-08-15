3e50db05839609151429cf413e927f1522776ede
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-08-15 19:29:35 +0000

    Mon, Aug 15, 2022, 7:29 PM +00:00

--- Week1/PSet1/mario-more/mario.c
+++ Week1/PSet1/mario-more/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int height;
+    do
+    {
+        int height = get_int("Enter a positive integer between 1 and 8. ");
+    }
+    while (height < 1 || height > 8);
+
+    for
 }
