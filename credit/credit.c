1e03423dcf30658df31f22709adf2c2dc73646ce
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-08-15 23:21:18 +0000

    Mon, Aug 15, 2022, 11:21 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -7,4 +7,13 @@ int main(void)
 
     //Checksum
     long checksum = credit % 100 * 2
+
+    if ()
+        printf("AMEX\n");
+    else if ()
+        printf("MASTERCARD\n");
+    else if ()
+        printf("VISA\n");
+    else
+        printf("INVALID\n");
 }
