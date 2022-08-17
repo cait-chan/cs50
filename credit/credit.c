64d1b9d53f5a27436d4e977730e60001106e681c
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-08-17 17:49:23 +0000

    Wed, Aug 17, 2022, 5:49 PM +00:00

--- credit/credit.c
+++ credit/credit.c
@@ -17,8 +17,15 @@ long checksum(long credit)
     for (long i = (credit / 10); i >= 1; i / 100)
     // where n is the number of digits
     {
-        for (int j = i % 10; j >= 0; j)
-            return int j = i % 10;
+        //for (int j = i % 10; j >= 0; j)
+        return int j = i % 10 * 2;
+        for (int j = j; j >= 0; j / 10)
+        {
+            return int k = j % 10
+            for (int k = k; k >= 0; k)
+        }
+
+
     }
 
     //return credit % 100 * 2;
