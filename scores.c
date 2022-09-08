8ec059e4be5d3c26879d0547c3c1cebf97538b07
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-08 16:31:07 +0000

    Thu, Sep 8, 2022, 4:31 PM +00:00

--- scores.c
+++ scores.c
@@ -8,3 +8,5 @@ int main(void)
 
     printf("Average: %f\n", (score1 + score2 + score3) / 3);
 }
+// to get precise average without throwing away decimal points
+// need to convert this into a float
