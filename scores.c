5e2280bdce259f3c7edbe9a30dc062ff54f7a22f
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-11 23:25:26 +0000

    Sun, Sep 11, 2022, 11:25 PM +00:00

--- scores.c
+++ scores.c
@@ -6,7 +6,11 @@ int main(void)
     int score2 = 73;
     int score3 = 33;
 
-    printf("Average: %f\n", (score1 + score2 + score3) / 3);
+    printf("Average: %f\n", (score1 + score2 + score3) / 3.0);
 }
 // to get precise average without throwing away decimal points
 // need to convert this into a float
+
+// this is why we divide by "3.0" because in C, as long as one
+// of the values participating in an arithetic expression is
+// a float, the rest will be promoted to floats as well
