7104268fb2bc32c4614fa1892c4a838c75b5eccb
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-12 00:59:45 +0000

    Mon, Sep 12, 2022, 12:59 AM +00:00

--- length.c
+++ length.c
@@ -1,4 +1,18 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main
+int main(void)
+{
+    string name = get_string("Name: ");
+
+    int i = 0;
+    while (name[i] != '\0')
+    {
+        i++;
+    }
+    // this while loop increases the value of i so long
+    // as it does not equal '\0'
+    printf("%i\n", i);
+
+}
+// this finds the length of the string
