1c8fd9a1c2b957b36bc99321b56897615e1015b5
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-12 17:23:24 +0000

    Mon, Sep 12, 2022, 5:23 PM +00:00

--- argv.c
+++ argv.c
@@ -4,7 +4,8 @@
 int main(int argc, string argv[])
 {
     if (argc == 2)
-    printf("hello, %s\n", argv[1]);
+    {
+        printf("hello, %s\n", argv[1]);
     //since argv records everything the human types,
     //argv[0] will be the name of the program run
     //ie. it will print "hello, ./argv"
@@ -12,4 +13,9 @@ int main(int argc, string argv[])
     //name typed immediately after the ./argv command
     //so that it will accept "./argv NAME" and
     //print "hello, NAME"
+    }
+    else
+    {
+        printf("hello, world\n");
+    }
 }
