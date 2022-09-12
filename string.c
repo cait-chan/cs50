39f8f2ce655146daf7559ed12291292010904843
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-12 05:09:09 +0000

    Mon, Sep 12, 2022, 5:09 AM +00:00

--- string.c
+++ string.c
@@ -6,14 +6,17 @@ int main(void)
 {
     string s = get_string("Input: ");
     printf("Output: ");
+    int length = strlen(s);
 
     //if you couldn't use %s in the printf function,
     //how else could you print out the string?
 
-    for (int i = 0; i < strlen(s); i++)
+    for (int i = 0; i < length; i++)
     //the string is never changing in length, so it's repetitive
     //to keep checking the condition by comparing i to the
     //value of the strlen function after its called
+        //it's optimal to store the strlen(s) in the variable length
+        //and then check the condition against length
     {
         printf("%c", s[i]);
     }
