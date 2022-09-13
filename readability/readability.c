eae794626020b4a1cdba384638f9bc38b07673be
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-13 23:49:03 +0000

    Tue, Sep 13, 2022, 11:49 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -77,9 +77,9 @@ int count_sentences(string text)
         {
             sentences += 1;
         }
-        else if ((text[i] - 46) == 0)
+        else if ((text[i] - 63) == 0)
         {
-            
+            sentences += 1;
         }
         sentences += 0;
     }
