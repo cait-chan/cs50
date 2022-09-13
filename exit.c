30fd1c6557192b4e3f77846f48f66253aa6286b5
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-13 01:18:59 +0000

    Tue, Sep 13, 2022, 1:18 AM +00:00

---
+++ exit.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+//want to write a program that will quit if argc is not
+//equal to 2
+//
+
+int main(int argc, string argv[])
+{
+    if (argc != 2)
+    {
+        printf("Missing command-line argument\n");
+        return
+    }
+}
