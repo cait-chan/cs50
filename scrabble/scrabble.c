1262a017459998eac7c7d27b0db816ff9ac82d98
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-13 18:02:32 +0000

    Tue, Sep 13, 2022, 6:02 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -52,8 +52,9 @@ int compute_score(string word)
             int place = (word[i]);
             score = POINTS[place];
         }
-        score = 0;
+        
         scoretotal += score;
+        score = 0;
     }
     return scoretotal;
 }
