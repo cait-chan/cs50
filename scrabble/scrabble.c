260c8d03d57aad75334930f762c5be7648062e72
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-13 18:27:13 +0000

    Tue, Sep 13, 2022, 6:27 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -41,19 +41,18 @@ int compute_score(string word)
     // TODO: Compute and return score for string
     for (int i = 0, n = strlen(word); i < n; i++)
     {
+        if (isalpha(word[i]))
+        {
         //want to move through the word, adding up the points
         //for each letter value
-        if (islower(word[i]))
-        {
-            place = ((toupper(word[i]))) - 65;
-        }
-        else if (isupper(word[i]))
-        {
-            place = (word[i]) - 65;
-        }
-        else
-        {
-            score = 0;
+            if (islower(word[i]))
+            {
+                place = ((toupper(word[i]))) - 65;
+            }
+            else if (isupper(word[i]))
+            {
+                place = (word[i]) - 65;
+            }
         }
         score = POINTS[place];
         scoretotal += score;
