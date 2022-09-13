11e22e4a95202fd1c21963239485bb8a08000c7e
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-13 17:21:21 +0000

    Tue, Sep 13, 2022, 5:21 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -40,7 +40,10 @@ int compute_score(string word)
     {
         //want to move through the word, adding up the points
         //for each letter value
-        if (islower)
+        if (islower(word[i]))
+        {
+            
+        }
     }
     return ()
 }
