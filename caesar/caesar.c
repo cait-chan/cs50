3d280cf28b8aa0ab2925bdf346a0787f5a37165a
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-14 05:49:04 +0000

    Wed, Sep 14, 2022, 5:49 AM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -6,8 +6,12 @@ int main(int argc, string argv[])
     //want to encrypt messages using Caesar's algorithm
     if (argc == 2)
     {
-        string plaintext = get_string("Plaintext:  ");
+        string plaintext = get_string("plaintext:  ");
+        for (i = 0, n = strlen(plaintext); i < n; i++)
+        {
 
+        }
+        printf("ciphertext: ")
     }
     printf("Usage: ./caesar key\n");
     return 1;
