66ffb2ae54bb8ffe7e22a8c105f1a1424f6b456b
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-14 00:52:30 +0000

    Wed, Sep 14, 2022, 12:52 AM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -43,7 +43,7 @@ int count_letters(string text)
     int letters = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
-        if (isalnum(text[i]))
+        if (isalpha(text[i]))
         {
             letters += 1;
         }
