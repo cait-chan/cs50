5844911e06e4ef2370e762bbf1b89855b4b54189
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-15 23:40:34 +0000

    Thu, Sep 15, 2022, 11:40 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -1,32 +1,36 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//want to encrypt a message by replacing every letter with another letter
+//the key will be a mapping of each of the letters of the alphabet to
+//the letter it should correspond to after the encryption
+
 int main(int argc, string argv[])
 {
-    //want to encrypt a message by replacing every letter with another letter
-    //the key will be a mapping of each of the letters of the alphabet to
-    //the letter it should correspond to after the encryption
     if (argc == 2)
         //the number of command-line arguments must be 2
         //one to run the program and the other to specify the key
     {
         if (isalpha(argv[]) = 0)
-        //this specifies that the key must be 26 alphabetical unique characters
+            //this specifies that the key must be alphabetical
         {
             printf("Key must only contain alphabetic characters.");
             return 1;
         }
         else if ()
+            //this specifies that the key must have each letter only once
         {
             printf("Key must not contain repeated characters.");
             return 1;
         }
         else if (strlen(argv[]) != 26)
+            //this specifies that the key must be 26 characters
         {
             printf("Key must contain 26 characters.");
             return 1;
         }
         else
+            //if the key fulfils all requirements, we can prompt the user for plaintext
         {
             string plaintext = get_string("Plaintext:  ");
         }
