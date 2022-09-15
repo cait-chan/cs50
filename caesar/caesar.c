887b246985b8066b6044ed99e6c7e2b33f3e57c4
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-15 21:17:08 +0000

    Thu, Sep 15, 2022, 9:17 PM +00:00

--- caesar/caesar.c
+++ caesar/caesar.c
@@ -14,7 +14,7 @@ int main(int argc, string argv[])
         if (only_digits(argv[1]))
         {
             return 0;
-            int 
+            int letter = atoi(argv[1]);
             string plaintext = get_string("plaintext:  ");
 
         }
@@ -40,3 +40,5 @@ bool only_digits(string plaintext)
     }
     return false;
 }
+
+char rotate(char c, int n)
