dfe5c99342c2fddc99d4bc297c2884e66024a8a4
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-15 23:38:23 +0000

    Thu, Sep 15, 2022, 11:38 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -10,15 +10,25 @@ int main(int argc, string argv[])
         //the number of command-line arguments must be 2
         //one to run the program and the other to specify the key
     {
-        if (isalpha(argv[]) && strlen(argv[]) == 26)
+        if (isalpha(argv[]) = 0)
         //this specifies that the key must be 26 alphabetical unique characters
         {
-            string plaintext = get_string("Plaintext:  ");
+            printf("Key must only contain alphabetic characters.");
+            return 1;
         }
-        else
+        else if ()
+        {
+            printf("Key must not contain repeated characters.");
+            return 1;
+        }
+        else if (strlen(argv[]) != 26)
         {
             printf("Key must contain 26 characters.");
             return 1;
+        }
+        else
+        {
+            string plaintext = get_string("Plaintext:  ");
         }
     }
     //this will print if there is no command-line argument or too many
