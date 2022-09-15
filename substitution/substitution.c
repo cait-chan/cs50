60facd98aecfe7fe736f398b3e41221f472fcb65
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-15 23:27:14 +0000

    Thu, Sep 15, 2022, 11:27 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -6,4 +6,20 @@ int main(int argc, string argv[])
     //want to encrypt a message by replacing every letter with another letter
     //the key will be a mapping of each of the letters of the alphabet to
     //the letter it should correspond to after the encryption
+    if (argc == 2)
+        //the number of command-line arguments must be 2
+        //one to run the program and the other to specify the key
+    {
+        if (isalpha(arg[]))
+        {
+
+        }
+        else
+        {
+            printf
+        }
+    }
+    //this will print if there is no command-line argument or too many
+    printf("Key must contain 26 characters.");
+    return 1;
 }
