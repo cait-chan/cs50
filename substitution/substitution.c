51935e5e700cff967c3f8e9882939a4cc76bcd74
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-15 23:22:20 +0000

    Thu, Sep 15, 2022, 11:22 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -3,5 +3,7 @@
 
 int main(int argc, string argv[])
 {
-    
+    //want to encrypt a message by replacing every letter with another letter
+    //the key will be a mapping of each of the letters of the alphabet to
+    //the letter it should correspond to after the encryption
 }
