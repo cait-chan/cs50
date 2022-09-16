45ef2651ffdc4f4863840953f1211cee1f8cafdc
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-16 22:56:34 +0000

    Fri, Sep 16, 2022, 10:56 PM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -11,7 +11,7 @@ string substitution(string s);
 
 int main(int argc, string argv[])
 {
-    string comparison = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    string comparison = (ABCDEFGHIJKLMNOPQRSTUVWXYZ);
     if (argc == 2)
         //the number of command-line arguments must be 2
         //one to run the program and the other to specify the key
