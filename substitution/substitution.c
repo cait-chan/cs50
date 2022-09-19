13052a69db2447762ead31123e1fa61542c21cfd
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-19 00:03:29 +0000

    Mon, Sep 19, 2022, 12:03 AM +00:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -78,12 +78,12 @@ bool unique(string s)
     int value = 0;
     string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    for (int i = 0, n = strlen(alphabet); i <= n; i++)
+    for (int i = 0, n = strlen(alphabet); i < n; i++)
     {
         return alphatotal += (int) alphabet[i];
     }
 
-    for (int i = 0, n = strlen(s); i <= n; i++)
+    for (int i = 0, n = strlen(s); i < n; i++)
     {
         return value += (int) s[i];
     }
