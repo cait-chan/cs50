ef52a330dc3b82845a96c59105771d5859c8f6d6
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-19 03:40:55 +0000

    Mon, Sep 19, 2022, 3:40 AM +00:00

--- substitution/unique.c
+++ substitution/unique.c
@@ -23,7 +23,7 @@ bool unique(string s)
     for (int i = 0, n = strlen(s); i < n; i++)
     {
         return key = s[i];
-        return total += key;
+        return key += key;
     }
 
     if (atoi(alphabet) == key)
