d9c08a0f63d96730b4244902d9baaeadc04a4c9e
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-22 21:59:33 +0000

    Thu, Sep 22, 2022, 9:59 PM +00:00

--- phonebook.c
+++ phonebook.c
@@ -2,11 +2,23 @@
 #include <stdio.h>
 #include <string.h>
 
+typedef struct
+{
+    string name;
+    string number;
+}
+person;
+
 int main(void)
 {
-    string names[] = {"Carter", "David"};
-    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
+    //string names[] = {"Carter", "David"};
+    //string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
     //difficult to use this using a large database when blind trust is relating the name to the phone number
+
+    person people[2];
+
+    person[0].name = "Carter"
+    //the dot means go inside the variable called name and give it the value of "Carter"
 
     for (int i = 0; i < 2; i++)
     {
