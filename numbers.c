d48e563e2db9be8418e0696b854a75e834bac70b
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-22 00:45:34 +0000

    Thu, Sep 22, 2022, 12:45 AM +00:00

--- numbers.c
+++ numbers.c
@@ -6,4 +6,15 @@ int main(void)
     int numbers[] = {4, 6, 8, 2, 7, 5, 0};
     //can use curly braces to define a set of values in an array
 
+    //this is an example of linear search
+    for (int i = 0; i < 7; i++)
+    {
+        if (numbers[i] == -1)
+        {
+            printf("Found\n");
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
