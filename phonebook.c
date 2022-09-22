3292f5d3c517293974656cb84ff1ce9bcdc4e0e2
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-22 21:47:41 +0000

    Thu, Sep 22, 2022, 9:47 PM +00:00

--- phonebook.c
+++ phonebook.c
@@ -5,5 +5,16 @@
 int main(void)
 {
     string names[] = {"Carter", "David"};
-    strings numbers[] = {"+1-617-495-}
+    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
+
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], "David") == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
 }
