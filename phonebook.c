f5d540c9a12dc73cdfc345bbac054b27701e69ab
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-22 22:04:31 +0000

    Thu, Sep 22, 2022, 10:04 PM +00:00

--- phonebook.c
+++ phonebook.c
@@ -17,14 +17,18 @@ int main(void)
 
     person people[2];
 
-    person[0].name = "Carter"
+    people[0].name = "Carter";
     //the dot means go inside the variable called name and give it the value of "Carter"
+    people[0].number = "+1-617-495-1000";
+
+    people[1].name = "David";
+    people[1].number = "+1-949-468-2750";
 
     for (int i = 0; i < 2; i++)
     {
-        if (strcmp(names[i], "David") == 0)
+        if (strcmp(people[i].name, "David") == 0)
         {
-            printf("Found %s\n", numbers[i]);
+            printf("Found %s\n", people[i].number);
             return 0;
         }
     }
