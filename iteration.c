1a7054e8db5a648c03524d1ccd0c8c54ec2e4344
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-24 07:56:43 +0000

    Sat, Sep 24, 2022, 7:56 AM +00:00

---
+++ iteration.c
@@ -0,0 +1,15 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    int height = get_int("Height: ");
+
+    draw(height);
+}
+
+void draw(int n)
+//since we only need the side effect of printing something, 
+{
+
+}
