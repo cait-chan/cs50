f57683e443d6c04b9aa75119b0a2ec276a2096dc
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-24 08:00:32 +0000

    Sat, Sep 24, 2022, 8:00 AM +00:00

--- iteration.c
+++ iteration.c
@@ -1,6 +1,10 @@
 #include <cs50.h>
 #include <stdio.h>
 
+//want to redo the mario problem from week 1 using recursion
+
+void draw(int n);
+
 int main(void)
 {
     int height = get_int("Height: ");
@@ -9,7 +13,15 @@ int main(void)
 }
 
 void draw(int n)
-//since we only need the side effect of printing something, 
+//since we only need the side effect of printing something, rather than returning a value,
+//we can put void before the function name
 {
-
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < i + 1; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
