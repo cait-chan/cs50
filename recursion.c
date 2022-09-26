356f2feb1b6d69df1b1ffb3865d651a4acec9649
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-26 22:59:20 +0000

    Mon, Sep 26, 2022, 10:59 PM +00:00

--- recursion.c
+++ recursion.c
@@ -5,12 +5,26 @@ void draw(int n);
 
 int main(void)
 {
-    int height = get_height("Height: ");
+    int height = get_int("Height: ");
 
     draw(height);
 }
 
 void draw(int n)
 {
+    if (n <= 0)
+    {
+        return;
+        //just saying return suffices because this function doesn't return a value
+    }
+    //this if statement is the base case which ensures that the function doesn't continue forever
 
+    draw(n - 1);
+    //this recursion calls these functions within itself first before continuing to the for loop
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("#");
+    }
+    printf("\n");
 }
