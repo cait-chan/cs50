d410df2d079a98226c3e1b27c25567319ed42fd1
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-27 00:00:34 +0000

    Tue, Sep 27, 2022, 12:00 AM +00:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -83,7 +83,7 @@ void print_winner(void)
 {
     for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = i + 1; j < candidate_count; j++)
+        for (int j = i + 1; j < candidate_count; j += 0)
         {
             if (candidates[i].votes > candidates[j].votes)
             {
