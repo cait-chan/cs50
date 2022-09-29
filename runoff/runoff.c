1aefccd5b418ab885a162044dded40e2c7ed61e8
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-29 19:07:21 +0000

    Thu, Sep 29, 2022, 7:07 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -178,7 +178,16 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    
+    int min = 0;
+
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (candidates[i].vote <= min)
+        {
+            return min = candidates[i].vote;
+        }
+        return min;
+    }
     return 0;
 }
 
