8650b5a67898a8702b5e19d597ce6569a2a37e58
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-09-29 19:02:43 +0000

    Thu, Sep 29, 2022, 7:02 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -169,6 +169,7 @@ bool print_winner(void)
         if (candidates[i].vote >= half_candidate)
         {
             printf("%s\n", candidates[i].name);
+            return true;
         }
     }
     return false;
