15824cc84febcd77f0d9e674f384494c0d8d6eb7
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-10-17 05:43:57 +0000

    Mon, Oct 17, 2022, 5:43 AM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -129,19 +129,14 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    for (int i = voter; i < voter_count; i++)
+    for (int j = 0; j < candidate_count; j++)
     {
-        rank = 0;
-        for (int j = 0; j < candidate_count; j++)
+        if (strcmp(candidates[j].name, name) == 0)
         {
-            if (strcmp(candidates[j].name, name) == 0)
-            {
-                preferences[i][rank] = j;
-                rank++;
-                return true;
-            }
-            return false;
+            preferences[voter][rank] = j;
+            return true;
         }
+        return false;
     }
     return false;
 }
