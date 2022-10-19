71255e483ff73e1f232109cf1f6f3b061497cea5
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-10-19 18:27:56 +0000

    Wed, Oct 19, 2022, 6:27 PM +00:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -167,7 +167,8 @@ bool print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes >= half_candidate && candidates[i].votes > winner_votes)
+        if (candidates[i].votes >= half_candidate)
+        if  (candidates[i].votes > winner_votes)
         {
             winner = candidates[i].name;
         }
