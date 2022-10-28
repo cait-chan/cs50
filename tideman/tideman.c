506b42e6215d5c74793833662245d77f5e230f50
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-10-28 01:18:13 +0000

    Fri, Oct 28, 2022, 1:18 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -131,9 +131,9 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    for (int i = 0; i <= candidate_count; i++)
+    for (int i = 0; i < candidate_count; i++)
     {
-        for (int j = 0; j <= candidate_count; j++)
+        for (int j = 0; j < candidate_count; j++)
         {
             for (int k = 0; k <= pair_count; k++)
             {
