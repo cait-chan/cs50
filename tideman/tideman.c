9cf06ce254a71da17448c001f0ee5164b8cfdcfb
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-10-28 01:28:21 +0000

    Fri, Oct 28, 2022, 1:28 AM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -119,7 +119,7 @@ void record_preferences(int ranks[])
     {
         for (int j = 0; j < candidate_count; j++)
         {
-            if (ranks[i] != ranks[j])
+            if (ranks[i] < ranks[j])
             {
                 preferences[i][j]++;
             }
