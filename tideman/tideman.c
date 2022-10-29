027968ef2971fca8a595721c49253f7d986b9801
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-10-29 22:39:13 +0000

    Sat, Oct 29, 2022, 10:39 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -151,6 +151,7 @@ void add_pairs(void)
 // Sort pairs in decreasing order by strength of victory
 void sort_pairs(void)
 {
+    //should utilize recurssion
     int strength = 0;
 
     for (int i = 0; i <= pair_count; i++)
