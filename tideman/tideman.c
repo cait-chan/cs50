4ed46fc575a54711567b170ae105c2e8271cbf81
cait-chan <76853159+cait-chan@users.noreply.github.com>
2022-10-29 22:08:39 +0000

    Sat, Oct 29, 2022, 10:08 PM +00:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -134,7 +134,7 @@ void add_pairs(void)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            for (int k = 0; k <= candidate_count; k++)
+            for (int k = 0; k <= (candidate_count! / 2); k++)
             //the maximum number of pairs that can exist is the number of candidates
             {
                 if (preferences[i][j] > 0)
