5c098a131ad84b9d88b89866ae1a5c760c8c007c
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-03-23 08:24:47 +0000

    Thu, Mar 23, 2023, 1:24 AM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -158,7 +158,7 @@ void sort_pairs(void)
     int i = 0;
     int j = 0;
     int k = 0;
-    pair temp[MAX * (MAX - 1) / 2];
+    pair temp = ;
     while (i < (pair_count - 1))
     {
         for (j = 0, k = 0; j <= (pair_count - 2); j++, k++)
