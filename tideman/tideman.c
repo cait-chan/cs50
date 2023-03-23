b989e27ac9b95d862044aaa53a195bd81e10235f
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-03-23 08:23:51 +0000

    Thu, Mar 23, 2023, 1:23 AM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -130,7 +130,6 @@ void record_preferences(int ranks[])
 // Record pairs of candidates where one is preferred over the other
 void add_pairs(void)
 {
-    printf(pairs);
     for (int i = 0; i < candidate_count; i++)
     {
         for (int j = i + 1; j < candidate_count; j++)
@@ -159,7 +158,7 @@ void sort_pairs(void)
     int i = 0;
     int j = 0;
     int k = 0;
-    pair temp = {};
+    pair temp[MAX * (MAX - 1) / 2];
     while (i < (pair_count - 1))
     {
         for (j = 0, k = 0; j <= (pair_count - 2); j++, k++)
