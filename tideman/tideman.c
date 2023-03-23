70a1c19633a1c7d1c37b2f69cb383b59ac837b01
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-03-23 07:57:21 +0000

    Thu, Mar 23, 2023, 12:57 AM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -156,22 +156,19 @@ void sort_pairs(void)
     //should utilize recurssion
 
     int i = 0;
-    int k = 0;
     while (i < (pair_count - 1))
     {
-        for (int j = 0; j <= (pair_count - 2); j++)
+        for (int j = 0, int k = 0; j <= (pair_count - 2), k <= (pair_count); j++, k++)
         {
             if ((pairs[j].winner - pairs[j].loser) >= (pairs[j+1].winner - pairs[j+1].loser))
             {
                 pairs[k] = pairs[j];
                 pairs[k+1] = pairs[j+1];
-                k++;
             }
             else if ((pairs[j].winner - pairs[j].loser) < (pairs[j+1].winner - pairs[j+1].loser))
             {
                 pairs[k] = pairs[j+1];
                 pairs[k+1] = pairs[j];
-                k++;
             }
         }
         i++;
