6bf88334af8808ec8ee4249da110b7c740591a85
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-02 03:28:54 +0000

    Sat, Apr 1, 2023, 8:28 PM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -180,7 +180,7 @@ void lock_pairs(void)
     {
         for (int j = i - 1; j >= 0; j--)
         {
-            if (pairs[i].loser != pairs[j].winner) || (pairs[i].loser != pairs[j].loser)
+            if (pairs[i].loser == pairs[j].winner)
             {
                 locked[pairs[i].winner][pairs[i].loser] = false;
             }
