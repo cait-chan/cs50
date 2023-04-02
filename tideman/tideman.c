c1b2965837259f3034a1251a08616b2f5555ad0a
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-02 02:53:02 +0000

    Sat, Apr 1, 2023, 7:53 PM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -174,7 +174,7 @@ void sort_pairs(void)
 // Lock pairs into the candidate graph in order, without creating cycles
 void lock_pairs(void)
 {
-    for (int i = 0; i < pair_count - 1; i++)
+    for (int i = 0; i < pair_count; i++)
     {
         if (pairs[i].winner == pairs[pair_count - i - 1].loser)
         {
