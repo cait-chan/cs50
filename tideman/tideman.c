05bd03da04da5a39e1fbab10ac78cee9048b1b79
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-02 02:35:01 +0000

    Sat, Apr 1, 2023, 7:35 PM -07:00

--- tideman/tideman.c
+++ tideman/tideman.c
@@ -161,11 +161,6 @@ void sort_pairs(void)
     {
         for (int j = 0; j < (pair_count - i - 1); j++)
         {
-            //if ((pairs[j].winner - pairs[j].loser) >= (pairs[j+1].winner - pairs[j+1].loser))
-            //{
-               // pairs[j] = pairs[j];
-                //pairs[j+1] = pairs[j+1];
-            //}
             if ((pairs[j].winner - pairs[j].loser) > (pairs[j+1].winner - pairs[j+1].loser))
             {
                 temp = pairs[j];
