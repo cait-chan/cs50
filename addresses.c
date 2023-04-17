43c744cc2972c9a9d131824171d2a30d05051723
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-17 23:55:22 +0000

    Mon, Apr 17, 2023, 4:55 PM -07:00

--- addresses.c
+++ addresses.c
@@ -5,3 +5,6 @@ int main(void)
     int n = 50;
     printf("%i\n", n);
 }
+
+//this means that the number 50 will end up in the computers memory and will be labelled "n"
+//this integer will take up 4 bytes
