b726e10d01b86ad76cf066e4a3690a396e69013b
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-18 00:45:13 +0000

    Mon, Apr 17, 2023, 5:45 PM -07:00

--- addresses.c
+++ addresses.c
@@ -1,11 +1,18 @@
+#include <cd50.h>
 #include <stdio.h>
 
-int main(void)
-{
-    int n = 50;
-    int *p = &n;
-    printf("%i\n", *p);
-}
+//int main(void)
+//{
+    //int n = 50;
+   // int *p = &n;
+   // printf("%i\n", *p);
+//}
 
 //this means that the number 50 will end up in the computers memory and will be labelled "n"
 //this integer will take up 4 bytes
+
+int main(void)
+{
+    string s = "HI!";
+    printf("%p\n", s);
+}
