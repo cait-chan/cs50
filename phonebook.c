b0222439d6b231fbe4c4f474c79a33153aff93b5
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-23 22:23:21 +0000

    Sun, Apr 23, 2023, 3:23 PM -07:00

--- phonebook.c
+++ phonebook.c
@@ -7,11 +7,16 @@
 int main(void)
 {
     FILE *file = fopen("phonebook.csv", "a");
+    //FILE is a new data type (specific to c), and this utilizes a pointer
+    //fopen is a new function which opens a file and returns a pointer there to in memory
+        //this will be called phonebook.csv and will be in append mode "a" so that we can keep adding more names and numbers
 
     string name = get_string("Name: ");
     string number = get_string("Number: ");
 
     fprintf(file, "%s,%s\n", name, number);
+    //fprintf prints to a file, rather than to the string
+    //hence, needing to start it with the file you want to print to: "file"
 
     fclose(file);
 }
