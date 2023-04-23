e3146b957d8afc86d73f5ffdfa0d96c0251e196b
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-23 22:17:51 +0000

    Sun, Apr 23, 2023, 3:17 PM -07:00

--- phonebook.c
+++ phonebook.c
@@ -6,5 +6,12 @@
 
 int main(void)
 {
-    
+    FILE *file = fopen("phonebook.csv", "a");
+
+    string name = get_string("Name: ");
+    string number = get_string("Number: ");
+
+    fprintf(file, "%s,%s\n", name, number);
+
+    fclose(file);
 }
