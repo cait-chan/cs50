f029eaad1301d18c1bb5246b2340db4296906414
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-23 20:44:06 +0000

    Sun, Apr 23, 2023, 1:44 PM -07:00

--- memory.c
+++ memory.c
@@ -6,6 +6,11 @@ int main(void)
     int *x = malloc(3 * sizeof(int));
     //this is asking for enough memory for an array of size 3
     //must use *x rather than x because we need to store the allocated memory in an address
+    if (x == NULL)
+    {
+        return 1;
+    }
+
     x[0] = 72;
     x[1] = 73;
     x[2] = 33;
