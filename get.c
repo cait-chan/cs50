b6f2a7e7e276d6575ccf77e8990468acf6a37f08
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-23 22:10:46 +0000

    Sun, Apr 23, 2023, 3:10 PM -07:00

--- get.c
+++ get.c
@@ -9,16 +9,19 @@ int main(void)
     int x;
     printf("x: ");
     scanf("%i", &x);
-    //this is a built in function for c which "scans" the users keyboard for input
-    //we have to store it in x, but must use & to denote the location by reference
+        //this is a built in function for c which "scans" the users keyboard for input
+        //we have to store it in x, but must use & to denote the location by reference
     printf("x: %i\n", x);
 }
 //this works pretty easily for integers, but how about strings?
 
 int main(void)
 {
-    char *s;
+    char *s = NULL;
+        //this is so that we initialize the value
     printf("s: ");
     scanf("%s", s);
-    //we don't need an & in front of s here because s is already declared as an address by using char *
+        //we don't need an & in front of s here because s is already declared as an address by using char *
+        //strings are special in this way
+    printf("s: %s\n", s);
 }
