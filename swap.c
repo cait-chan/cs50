93866ebbaa516b1889a86bcb8d5973cd1731402a
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-04-23 21:58:52 +0000

    Sun, Apr 23, 2023, 2:58 PM -07:00

--- swap.c
+++ swap.c
@@ -20,3 +20,25 @@ void swap(int a, int b)
 }
 //this does not correctly swap x and y because in c when you call a function and pass in two arguments (a and b),
 //you're passing in those argument by value only (ie. a copy, not the actual variable)
+
+void swap(int *a, int *b);
+
+int main(void)
+{
+    int x = 1;
+    int y = 2;
+
+    printf("x is %i, y is %i\n", x, y);
+    swap(&x, &y);
+    //we need to add the & in here because we are entering the addresses of x and y
+    printf("x is %i, y is %i\n", x, y);
+}
+
+void swap(int *a, int *b)
+{
+    int tmp = *a;
+    *a = *b;
+    *b = tmp;
+}
+//this now utilizes * to dereference and go to the values stored at the variables, which are the addresses since we use &
+//when passing through the variables into the function in main
