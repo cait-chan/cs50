b1b6cfb528fc3d3bfeac644966bc5b17080eced7
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-05-02 22:42:24 +0000

    Tue, May 2, 2023, 3:42 PM -07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -3,4 +3,14 @@
 void colorize(int height, int width, RGBTRIPLE image[height][width])
 {
     // Change all black pixels to a color of your choosing
+    for (i = 0, i < height, i++)
+    {
+        for (j = 0, j < width, j++)
+        {
+            if (pixel.rgbtBlue == 0x00, pixel.rgbtGreen == 0x00, pixel.rgbtRed == 0x00)
+            {
+                pixel.rgbt
+            }
+        }
+    }
 }
