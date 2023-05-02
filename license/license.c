284d9b51ee036b96aea1313fa7d11ebd1c0f66b2
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-05-02 21:52:47 +0000

    Tue, May 2, 2023, 2:52 PM -07:00

--- license/license.c
+++ license/license.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
     }
 
     // Create buffer to read into
-    char buffer[7];
+    char *buffer[7] = malloc(7);
 
     // Create array to store plate numbers
     char *plates[8];
