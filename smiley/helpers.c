6aa76cabd650bb8673f449e6423d80c606f36cec
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-05-02 22:49:28 +0000

    Tue, May 2, 2023, 3:49 PM -07:00

--- smiley/helpers.c
+++ smiley/helpers.c
@@ -7,9 +7,9 @@ void colorize(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j <= width; j++)
         {
-            if (pixel.rgbtBlue == 0x00, pixel.rgbtGreen == 0x00, pixel.rgbtRed == 0x00)
+            if (image[i][j].rgbtBlue == 0x00 && image[i][j].rgbtGreen == 0x00 && image[i][j].rgbtRed == 0x00)
             {
-                pixel.rgbtBlue = 0xFD, pixel.rgbtGreen = 0xCA, pixel.rbgtRed = 0xDE;
+                image[i][j].rgbtBlue = 0xFD && image[i][j].rgbtGreen = 0xCA && image[i][j].rgbtRed = 0xDE;
             }
         }
     }
