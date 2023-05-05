4804315f7585941d1429e565893eacd5410b0702
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-05-05 22:08:34 +0000

    Fri, May 5, 2023, 3:08 PM -07:00

--- volume/volume.c
+++ volume/volume.c
@@ -39,13 +39,7 @@ int main(int argc, char *argv[])
     // TODO: Copy header from input file to output file
     while (fread(header, sizeof(HEADER_SIZE), 1, input))
     {
-        
-    }
-
-
-    for (int i = 0, i < 44, i++)
-    {
-        header[i] = (argv[1])[i];
+        fwrite(header, sizeof(HEADER_SIZE), 1, output);
     }
 
     // TODO: Read samples from input file and write updated data to output file
