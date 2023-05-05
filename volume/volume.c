051ed80369beaaf9b5e358f881b13636998a2ab7
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-05-05 21:55:11 +0000

    Fri, May 5, 2023, 2:55 PM -07:00

--- volume/volume.c
+++ volume/volume.c
@@ -34,7 +34,8 @@ int main(int argc, char *argv[])
     float factor = atof(argv[3]);
 
     // TODO: Copy header from input file to output file
-    char *
+    char *header = malloc(sizeof(HEADER_SIZE));
+    strcpy(header, )
     // TODO: Read samples from input file and write updated data to output file
 
     // Close files
