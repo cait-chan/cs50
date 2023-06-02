5fea1c20fcea250a20832cd09fef162c947528bb
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-06-02 21:21:09 +0000

    Fri, Jun 2, 2023, 2:21 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
 {
  if (argc != 2)
  {
-    printf("Usage: ./recover file\n");
+    printf("Usage: ./recover IMAGE\n");
     return 1;
  }
 
@@ -16,7 +16,7 @@ int main(int argc, char *argv[])
  }
 
 //create buffer to read into
-char buffer[512];
+int buffer[512];
 
 //create array to store
 char *image[50];
