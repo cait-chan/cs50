f07432ff68194dde8e3ab8116a76d75dea515a56
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-06-02 19:56:50 +0000

    Fri, Jun 2, 2023, 12:56 PM -07:00

---
+++ recover/recover.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+ if (argc != 2)
+ {
+    printf("Please enter only one command-line argument.\n");
+    return 1;
+ }
+
+ if (!fopen(argv[1]))
+ {
+    printf("Cannot open file for reading.\n");
+    return 1;
+ }
+
+FILE *file = fopen(argv[1], "r");
+
+ while (fread(buffer, 1, 512, file) == 512)
+ {
+
+ }
+}
