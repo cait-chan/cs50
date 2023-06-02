413b62f2ea0ca6ab74c2a3cc838bfc730b0084ba
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-06-02 21:56:10 +0000

    Fri, Jun 2, 2023, 2:56 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -33,7 +33,7 @@ int idx = 0;
 
 FILE *file = fopen(argv[1], "r");
 
- while (fread(buffer, 1, 512, file) == 512)
+ while (fread(buffer, 1, 512, file) != 0)
  {
    //if start of new JPEG
    if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
