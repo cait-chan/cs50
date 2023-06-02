bdfffa6c1721ef53217bf70b478febe3b866a97e
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-06-02 23:21:59 +0000

    Fri, Jun 2, 2023, 4:21 PM -07:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -10,7 +10,11 @@ int get_block_size(WAVHEADER header);
 int main(int argc, char *argv[])
 {
     // Ensure proper usage
-    // TODO #1
+    if (argc != 3)
+    {
+        printf("Usage: ./reverse input.WAV output.WAV\n")
+        return 1;
+    }
 
     // Open input file for reading
     // TODO #2
