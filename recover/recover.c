e3a1720082b31cb3491fa6ad0b94fff917e4d132
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-06-02 23:08:04 +0000

    Fri, Jun 2, 2023, 4:08 PM -07:00

--- recover/recover.c
+++ recover/recover.c
@@ -5,56 +5,56 @@ typedef uint8_t BYTE;
 
 int main(int argc, char *argv[])
 {
- if (argc != 2)
- {
-    printf("Usage: ./recover IMAGE\n");
-    return 1;
- }
-
-FILE *file = fopen(argv[1], "r");
- if (file == NULL)
- {
-    printf("Cannot open file for reading.\n");
-    return 1;
- }
-
-//create buffer to read into
-BYTE buffer[512];
-
-//create array to store
-char image[8] = {0};
-
-//initialize file pointer for new image files
-FILE *img = NULL;
-
-int idx = 0;
-
- while (fread(buffer, 1, 512, file) == 512)
- {
-   //if start of new JPEG
-   if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
+   if (argc != 2)
    {
-      //if this is the first JPEG
+      printf("Usage: ./recover IMAGE\n");
+      return 1;
+   }
+
+   FILE *file = fopen(argv[1], "r");
+   if (file == NULL)
+   {
+      printf("Cannot open file for reading.\n");
+      return 1;
+   }
+
+   //create buffer to read into
+   BYTE buffer[512];
+
+   //create array to store
+   char image[8] = {0};
+
+   //initialize file pointer for new image files
+   FILE *img = NULL;
+
+   int idx = 0;
+
+   while (fread(buffer, 1, 512, file) == 512)
+   {
+      //if start of new JPEG
+      if ((buffer[0] == 0xff) && (buffer[1] == 0xd8) && (buffer[2] == 0xff) && ((buffer[3] & 0xf0) == 0xe0))
+      {
+         //if this is the first JPEG
+         if (img != NULL)
+         {
+            fclose(img);
+         }
+
+         //make new JPEG file to write this data into
+         sprintf(image, "%03i.jpg", idx++);
+         img = fopen(image, "w");
+      }
+      //if already found JPEG and need to continue writing into same file
       if (img != NULL)
       {
-         fclose(img);
+         fwrite(buffer, 1, 512, img);
       }
-
-      //make new JPEG file to write this data into
-      sprintf(image, "%03i.jpg", idx++);
-      img = fopen(image, "w");
    }
-   //if already found JPEG and need to continue writing into same file
    if (img != NULL)
    {
-      fwrite(buffer, 1, 512, img);
+      fclose(img);
    }
- }
- if (img != NULL)
- {
-   fclose(img);
- }
- fclose(file);
+   fclose(file);
 
- return 0;
+   return 0;
 }
