ae29ba5a39af07151f3f740a08b1887a79b22b32
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-06-03 01:57:48 +0000

    Fri, Jun 2, 2023, 6:57 PM -07:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -59,8 +59,9 @@ int main(int argc, char *argv[])
 
     while (fread(buffer, 1, block_size, input))
     {
+        fseek()
         fwrite(buffer, 1, block_size, output)
-        
+
     }
 
     fclose(input);
