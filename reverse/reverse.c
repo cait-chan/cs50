fa477be5367ab5eb2a13fdf88e714b01b1f46163
cait-chan <76853159+cait-chan@users.noreply.github.com>
2023-06-03 01:47:39 +0000

    Fri, Jun 2, 2023, 6:47 PM -07:00

--- reverse/reverse.c
+++ reverse/reverse.c
@@ -49,7 +49,7 @@ int main(int argc, char *argv[])
 
     // Use get_block_size to calculate size of block
     // TODO #7
-    int block = get_block_size(header);
+    int block_size = get_block_size(header);
 
     // Write reversed audio to file
     // TODO #8
